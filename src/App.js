321778e15ad20b9cb9d4504352ccd0a3ca077125
celestroccayoli <celestetroccayoli@gmail.com>
2023-03-19 12:32:25 -0300

    sacamos seccion de prueba about

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import Header from "./components/header/Header";
 import History from "./components/history/History";
 import Products from "./components/products/Products";
+import Contact from "./components/contact/Contact"
 import {Route, Routes} from 'react-router-dom'
-
 import './index.css'
-import About from "./components/about/About";
+
 function App() {
   return (
     <div className="body-app" >
     <Header/>
-    <About/>
     <History id='historia'/>
     <Products id='productos' />
+    <Contact/>
     {/* <Routes>
       <Route path="/" exact />
       <Route path='/historia' element={<History />}/>
