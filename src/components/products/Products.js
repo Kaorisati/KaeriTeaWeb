254c362aa7f19702e12cd9c1849ea91e78a8657f
celestroccayoli <celestetroccayoli@gmail.com>
2023-03-19 12:24:10 -0300

    seccion products

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,17 +1,27 @@
 import React from "react";
+import "./Products.css"
+
 
 export default function Products() {
-    return (
-    <div>
-        <h3>EN CADA TAZA, UNA HISTORIA</h3>
-        <p>Productos Kaeriyama, en cada mesa.</p>
-    
-    <div>
-        <h4>TÉ NEGRO</h4>
-        <h4>TÉ VERDE</h4>
-        <h4>YERBA MATE</h4>
-    </div>
+  return (
+    <div className="section-products">
+      <h2 className="title2">EN CADA TAZA, UNA HISTORIA</h2>
+      <p className="description2">Productos Kaeriyama, en cada mesa.</p>
 
+      <div className="content-products">
+        <div className="card products">
+        <img src="/product2.png" alt="historia" className='img-product' />
+          <h4 className="title-product">TÉ NEGRO</h4>
+        </div>
+        <div className="card products">
+        <img src="/product1.png" alt="historia" className='img-product' />
+          <h4 className="title-product">TÉ VERDE</h4>
+        </div>
+        <div className="card products">
+        <img src="/product3.png" alt="historia" className='img-product' />
+          <h4 className="title-product">YERBA MATE</h4>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
