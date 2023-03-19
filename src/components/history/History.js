cfaf541338cf9f21f837f482f9ccc011f9fcec0d
celestroccayoli <celestetroccayoli@gmail.com>
2023-03-19 11:38:42 -0300

    seccion historia

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "./History.css"
+
 
 export default function History() {
-    return (
-    <div className="hist-container">Hola Historyyy</div>
-    )
+  return (
+    <div className="hist-container">
+      <div className="box-txt">
+        <p className="year description2">1921</p>
+        <h2 className="title2">EL SABOR DE LA HISTORIA</h2>
+        <p className="description2">
+          Comienza una historia de desafíos y apuesta a persistir en la selva
+          Misionera de entonces. Desde Japón traen consigo los valores de su
+          cultura, sobre todo la perseverancia, que lo han aplicado en todas las
+          actividades que emprendieron. Luego de un siglo y tres generaciones,
+          estos valores permanecen como la herencia más valiosa de la familia
+          Kaeriyama.
+        </p>
+      </div>
+      <div className="img-history">
+      <img src="/img-history.png" alt="historia" className='img-history' />
+      </div>
+    </div>
+  );
 }
