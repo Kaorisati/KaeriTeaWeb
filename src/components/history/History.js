d1d6eda3a3d5b999e847266a17c3cd41efe01f63
Kaori Kairiyama <kaorisati@gmail.com>
2023-03-19 11:39:03 -0300

    history y products

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,6 +1,20 @@
 
 export default function History() {
     return (
-    <div className="hist-container">Hola Historyyy</div>
+    <div className="hist-container">
+        <p>1921</p>
+        <h4> EL SABOR DE LA HISTORIA</h4>
+        <p>Comienza una historia de desafíos y apuesta a persistir 
+            en la selva Misionera de entonces.
+            Desde Japón traen consigo los valores de su cultura, 
+            sobre todo la perseverancia, que lo han aplicado en 
+            todas las actividades que emprendieron. Luego de un 
+            siglo y tres generaciones, estos valores permanecen 
+            como la herencia más valiosa de la familia Kaeriyama.</p>
+        <p>2021</p>
+        <p>Hoy queremos brindarles a través de nuestros productos 
+        un pedacito de nuestra historia y dedicación a lo que 
+        hacemos, en cada paquete, taza, en cada mesa.</p>
+    </div>
     )
 }
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,6 +1,16 @@
 
 export default function Products() {
     return (
-    <div>Hola Products</div>
+    <div>
+        <h3>EN CADA TAZA, UNA HISTORIA</h3>
+        <p>Productos Kaeriyama, en cada mesa.</p>
+    
+    <div>
+        <h4>TÉ NEGRO</h4>
+        <h4>TÉ VERDE</h4>
+        <h4>YERBA MATE</h4>
+    </div>
+
+    </div>
     )
 }
